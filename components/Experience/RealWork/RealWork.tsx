da4ca6bc3699e1c61bcf7cfdc1cc97fc902928f6
Metik Vladislav <68441098+destolyar@users.noreply.github.com>
2023-01-09 12:28:06 +0300

    Info update

--- components/Experience/RealWork/RealWork.tsx
+++ components/Experience/RealWork/RealWork.tsx
@@ -31,7 +31,7 @@ export const RealWork = () => {
       <RealWorkItem 
         companyType='Telecommunication company' 
         label='Business network'
-        date='January 2022 - August 2022'
+        date='June 2022 - August 2022'
         image='/companies/buisness-network.jpg'
         cutImage={true}
         workTasks={[
